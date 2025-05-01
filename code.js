829f1a760ca8d56bd88876a4dd72f9f4c5a99ec3
michael1303 <88162384+michael1303@users.noreply.github.com>
2025-05-01 12:03:45 -0400

    add function that iterates over "bookArray" and manipulates DOM to display books within HTML table

--- code.js
+++ code.js
@@ -1,4 +1,7 @@
 
+const tableBody = document.querySelector("tbody");
+
+
 const myLibrary = [
     {
         id: 5,
@@ -21,8 +24,48 @@ function Book(id, title, author, pages, read) {
     this.read = read;
 }
 
+
 function addBookToLibrary(title, author, pages, read){
     myLibrary.push(crypto.randomUUID(), title, author, pages, read);
+}
+
+
+function displayBooks(bookArray){
+
+    // creating DOM fragment
+    const tableRows = document.createDocumentFragment();
+
+    // iteratively adding new book rows to DOM fragment
+    for (let i = 0; i < bookArray.length; i++){
+
+        // creating row
+        const row = tableBody.appendChild(document.createElement("tr"));
+
+        // adding id
+        const id = row.appendChild(document.createElement("td"));
+        id.textContent = bookArray[i].id;
+        
+        // adding title
+        const title = row.appendChild(document.createElement("td"));
+        title.textContent = bookArray[i].title;
+
+        // adding author
+        const author = row.appendChild(document.createElement("td"));
+        author.textContent = bookArray[i].author;
+
+        // adding pages
+        const pages = row.appendChild(document.createElement("td"));
+        pages.textContent = bookArray[i].pages;
+
+        // adding read
+        const read = row.appendChild(document.createElement("td"));
+        read.textContent = bookArray[i].read;
+
+    }
+
+    // Appending fragment to HTML
+    tableBody.appendChild(tableRows);
+    
 }
 
 
