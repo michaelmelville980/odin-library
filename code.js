ea70ed664d6d9a8b2dca2df6852fd7b26ea71265
michael1303 <88162384+michael1303@users.noreply.github.com>
2025-05-01 11:20:48 -0400

    Create Book constructor. Add function to initialize new book object.

--- code.js
+++ code.js
@@ -1,11 +1,18 @@
 
 const myLibrary = [];
 
-function Book() {
-    // the constructor...
+function Book(id, title, author, pages, read) {
+    if (!new.target){
+        throw Error ("You must use the 'new' operator");
+    }
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
-function addBookToLibrary(){
-    // take params, create a book then store it in the array
+function addBookToLibrary(title, author, pages, read){
+    myLibrary.push(crypto.randomUUID(), title, author, pages, read);
 }
 
